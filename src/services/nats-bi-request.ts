cfa1f2a90c2fa81fe3581bd932c85bff4199b82f
shekhar.s <shekhar7x@gmail.com>
2020-07-15 18:55:29 +0530

    fix

--- src/services/nats-bi-request.ts
+++ src/services/nats-bi-request.ts
@@ -1,5 +1,5 @@
-import { Client, connect, MsgCallback, Subscription } from 'ts-nats';
-import { BiRequest, BiRequestConfig } from '../types/bi-request';
+import { Client, connect, MsgCallback, Subscription, Msg } from 'ts-nats';
+import { BiRequest, BiRequestConfig, RequestPayload } from '../types/bi-request';
 
 export class NatsBiRequest implements BiRequest {
     subsciptions: Subscription[] = [];
@@ -11,6 +11,8 @@ export class NatsBiRequest implements BiRequest {
     }
 
     async initiate(): Promise<any> {
+        if (this.nc) return this.nc;
+
         this.nc = await connect(this.config.connectionUri);
         this.nc.on('error', (err) => {
             console.log(`[Nats error]`, err);
@@ -18,13 +20,46 @@ export class NatsBiRequest implements BiRequest {
         return this.nc;
     }
 
-    async consume(topic: string, callbackFn: MsgCallback): Promise<any> {
-        const sub = await this.nc.subscribe(topic, callbackFn, { queue: this.config.queueGroup });
+    getHandlerCallBack(handler: Function) {
+        return (payload?: RequestPayload, replyTo?: string): void => {
+            handler(payload)
+                .then((result) => {
+                    this.nc.publish(replyTo, JSON.stringify({ result: result }));
+                })
+                .catch((e) => {
+                    this.nc.publish(replyTo, JSON.stringify({ error: JSON.stringify(e) }));
+                });
+        };
+    }
+
+    async serve(path: string, handler: Function): Promise<any> {
+        this.nc || (await this.initiate());
+        const handlerCallback = this.getHandlerCallBack(handler);
+        const cb: MsgCallback = (err, message: Msg) => {
+            try {
+                if (err) throw err;
+
+                const payload: RequestPayload = JSON.parse(message.data);
+
+                handlerCallback(payload, message.reply);
+            } catch (e) {
+                console.log('[nats:subscription:error]', err);
+                if (message && message.reply) {
+                    this.nc.publish(message.reply, { error: JSON.stringify(err) });
+                }
+            }
+        };
+        const sub = await this.nc.subscribe(path, cb, { queue: this.config.queueGroup });
         this.subsciptions.push(sub);
     }
 
-    async request(path: string, message: { [key: string]: any }): Promise<any> {
-        const result = await this.nc.request(path, 6000, JSON.stringify(message));
+    async request(path: string, payload: RequestPayload): Promise<any> {
+        this.nc || (await this.initiate());
+        const response = await this.nc.request(path, 6000, JSON.stringify(payload));
+        const { error, result } = JSON.parse(response.data);
+
+        if (error) throw Object.assign(new Error(), error);
+
         return result;
     }
 }
--- src/types/bi-request.ts
+++ src/types/bi-request.ts
@@ -4,6 +4,12 @@ export interface BiRequestConfig {
     queueGroup?: string;
 }
 
+export interface RequestPayload {
+    body?: any;
+    params?: any;
+    query?: any;
+}
+
 export interface BiRequest {
     config?: BiRequestConfig;
     initiate(): Promise<any>;
